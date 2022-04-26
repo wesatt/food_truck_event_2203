68de2c13d15de47bd5fb29abce1d966776d2f260
Wesley Garcia <wes@alltradestech.com>
2022-04-26 09:05:17 -0700

    Implement Event#food_trucks_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -14,4 +14,10 @@ class Event
   def food_truck_names
     @food_trucks.map { |food_truck| food_truck.name }
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |food_truck|
+      food_truck.inventory.include?(item)
+    end
+  end
 end
