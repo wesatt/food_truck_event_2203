b43acbfc19625268ab706d7590891002be7f92dd
Wesley Garcia <wes@alltradestech.com>
2022-04-26 10:13:11 -0700

    Implement Event#overstocked_items

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,23 @@ class Event
     item_arr = @food_trucks.map { |food_truck| food_truck.inventory.keys }.flatten.uniq
     item_arr.sort_by { |item| item.name }
   end
+
+  def overstocked_items
+    items = @food_trucks.map { |food_truck| food_truck.inventory.keys }.flatten
+    dupes = items.find_all do |item|
+      item if items.count(item) > 1
+    end.uniq
+    duplicate_quantities = Hash.new(0)
+    dupes.each do |item|
+      duplicate_quantities[item]
+      @food_trucks.each do |food_truck|
+        duplicate_quantities[item] += food_truck.inventory[item] if !food_truck.inventory[item].nil?
+      end
+    end
+    overstocked = []
+    duplicate_quantities.each do |item, total|
+      overstocked << item if total > 50
+    end
+    overstocked
+  end
 end
