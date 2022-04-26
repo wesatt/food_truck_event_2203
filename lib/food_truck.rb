47ad8ad18b05062b4594d61bee34cd5ef1dccb2b
Wesley Garcia <wes@alltradestech.com>
2022-04-26 09:19:18 -0700

    Implement FoodTruck#potential_revenue; complete iteration 2

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -14,4 +14,10 @@ class FoodTruck
   def stock(item, qty)
     @inventory[item] += qty
   end
+
+  def potential_revenue
+    @inventory.sum do |item, qty|
+      item.price * qty
+    end
+  end
 end
