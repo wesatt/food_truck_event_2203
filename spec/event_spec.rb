4b880ccd5ec835b866d31f20c7010efc1f1ae5b4
Wesley Garcia <wes@alltradestech.com>
2022-04-26 09:39:04 -0700

    Add test for Event#sorted_items_list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -51,4 +51,34 @@ RSpec.describe Event do
       expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
     end
   end
+
+  describe "iteration 3" do
+    let(:event) { Event.new("South Pearl Street Farmers Market") }
+    let(:item1) { Item.new({name: "Peach Pie (Slice)", price: "$3.75"}) }
+    # <Item:0x007f9c56740d48...>
+    let(:item2) { Item.new({name: "Apple Pie (Slice)", price: "$2.50"}) }
+    # <Item:0x007f9c565c0ce8...>
+    let(:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"}) }
+    # <Item:0x007f9c562a5f18...>
+    let(:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"}) }
+    # <Item:0x007f9c56343038...>
+    let(:food_truck1) { FoodTruck.new("Rocky Mountain Pies") }
+    let(:food_truck2) { FoodTruck.new("Ba-Nom-a-Nom") }
+    let(:food_truck3) { FoodTruck.new("Palisade Peach Shack") }
+    before :each do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+    end
+
+    it "can return all available items in a sorted list" do
+      expect(event.sorted_item_list).to eq([item2, item4, item1, item3])
+    end
+  end
 end
