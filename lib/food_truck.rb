6258f54513178e7061d4a46545d3e3c0a62c1447
Wesley Garcia <wes@alltradestech.com>
2022-04-26 08:28:29 -0700

    Add test for FoodTruck#check_stock and #stock

---
+++ lib/food_truck.rb
@@ -0,0 +1,9 @@
+class FoodTruck
+  attr_reader :name,
+    :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,7 +2,7 @@ require "./lib/item"
 require "./lib/food_truck"
 require "./lib/event"
 
-RSpec.describe Foodtruck do
+RSpec.describe FoodTruck do
   describe "iteration 1" do
     let(:item1) { Item.new({name: "Peach Pie (Slice)", price: "$3.75"}) }
     let(:item2) { Item.new({name: "Apple Pie (Slice)", price: "$2.50"}) }
@@ -11,6 +11,23 @@ RSpec.describe Foodtruck do
     it "exists and has attributes" do
       expect(food_truck.name).to eq("Rocky Mountain Pies")
       expect(food_truck.inventory).to eq({})
+    end
+
+    it "can check stock of and add qty to food items" do
+      expect(food_truck.check_stock(item1)).to eq(0)
+
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq({item1: 30})
+      expect(food_truck.check_stock(item1)).to eq(30)
+
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(55)
+
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.inventory).to eq({item1: 55, item2: 12})
     end
   end
 end
